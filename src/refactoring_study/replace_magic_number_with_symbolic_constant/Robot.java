c553a027028d9c12469c2a46cf9e7e8d1417bcc0
변지영 <bjy8668@hanmail.net>
2019-08-26 15:35:00 +0900

    상수로 치환 적용

--- src/refactoring_study/replace_magic_number_with_symbolic_constant/Robot.java
+++ src/refactoring_study/replace_magic_number_with_symbolic_constant/Robot.java
@@ -1,6 +1,9 @@
 package refactoring_study.replace_magic_number_with_symbolic_constant;
 
-public class Robot {
+public class Robot { //52p
+	public static final int COMMAND_JUMP = 2;
+	public static final int COMMAND_STOP = 1;
+	public static final int COMMAND_WALK = 0; //public이기 때문에 main에서도 접근 가능하다
 	private final String name;
 
 	public Robot(String name) {
@@ -9,11 +12,11 @@ public class Robot {
 	}
 	
 	public void order(int command) {
-		if(command == 0) {
+		if(command == COMMAND_WALK) {
 			System.out.printf("%s %s%n", name, "walks.");
-		}else if(command == 1) {
+		}else if(command == COMMAND_STOP) {
 			System.out.printf("%s %s%n", name, "stops.");
-		}else if(command == 2) {
+		}else if(command == COMMAND_JUMP) {
 			System.out.printf("%s %s%n", name, "jumps.");
 		}else {
 			System.out.printf("command error. command = %s%n", command);
